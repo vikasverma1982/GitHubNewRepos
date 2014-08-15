c11ae387ea5bf93934761471e87dc662fd153d17
vikas <vikasverma1982@yahoo.com>
2014-08-15 18:55:55 +0530

    r2 index changes for bug fixing

--- button1.c
+++ button1.c
@@ -5,7 +5,7 @@ int main()
 	printf("Hello World\n");
 	printf("Welcome\n");
 	printf("New Welcome\n");
-	printf("Uploaded to github\n");.
+	printf("Uploaded to github\n");
 	printf("Bug fix\n");
 	return 0;
 }
